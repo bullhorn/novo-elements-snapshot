8208c3b72741b1358e56cc9741d6f967f563b49f
Nadia Harris-Martinez <131195516+nadia-harrismartinez@users.noreply.github.com>
2024-02-02 17:41:45 +0000

    6c910ed Merge b96af6e05e04e5c78f3d804533cab1686e570dbe into 6ce48a8a783442c24749c8c21cae2dcc1d8ff0a3

---
+++ elements/avatar/Avatar.d.ts
@@ -0,0 +1,22 @@
+import { OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as i0 from "@angular/core";
+export declare class NovoAvatarElement implements OnInit {
+    private sanitizer;
+    source: any;
+    label: string;
+    theme: string;
+    image: string;
+    size: string;
+    shape: string;
+    color: string;
+    get hb_classBinding(): string[];
+    get background(): string;
+    src: any;
+    constructor(sanitizer: DomSanitizer);
+    ngOnInit(): any;
+    setPrefixedValue(elm: any, prop: any, value: any): any;
+    private _isValidURL;
+    static ɵfac: i0.ɵɵFactoryDeclaration<NovoAvatarElement, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<NovoAvatarElement, "novo-avatar", never, { "source": "source"; "label": "label"; "theme": "theme"; "image": "image"; "size": "size"; "shape": "shape"; "color": "color"; }, {}, never, never>;
+}
