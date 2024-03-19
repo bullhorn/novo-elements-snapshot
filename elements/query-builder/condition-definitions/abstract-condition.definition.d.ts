ed236c5f1ba2596a978433ae5dad7b587f4f4db0
Dan Voegelin <dvoegelin@bullhorn.com>
2024-03-19 13:51:18 +0000

    1e9cbf5 Merge 41f104a882a563d9cdcd9981d55f7ece2d93df0b into b37e8f4d59b137f9df16833c450504a26ea15154

---
+++ elements/query-builder/condition-definitions/abstract-condition.definition.d.ts
@@ -0,0 +1,23 @@
+import { OnDestroy, OnInit } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+import { NovoLabelService } from 'novo-elements/services';
+import { NovoConditionFieldDef } from '../query-builder.directives';
+import * as i0 from "@angular/core";
+export declare abstract class AbstractConditionFieldDef implements OnDestroy, OnInit {
+    labels: NovoLabelService;
+    /** Column name that should be used to reference this column. */
+    get name(): string;
+    set name(name: string);
+    _name: string;
+    defaultOperator: string;
+    fieldDef: NovoConditionFieldDef;
+    constructor(labels: NovoLabelService);
+    ngOnInit(): void;
+    ngOnDestroy(): void;
+    onOperatorSelect(formGroup: UntypedFormGroup): void;
+    /** Synchronizes the column definition name with the text column name. */
+    private _syncFieldDefName;
+    private _syncFieldDefOperatorValue;
+    static ɵfac: i0.ɵɵFactoryDeclaration<AbstractConditionFieldDef, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<AbstractConditionFieldDef, never, never, { "name": { "alias": "name"; "required": false; }; }, {}, never, never, false, never>;
+}
