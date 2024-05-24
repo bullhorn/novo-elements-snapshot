31e2ab9b160b9b88fcd75bcdc1c92fd736b95cdf
Dan Voegelin <dvoegelin@bullhorn.com>
2024-05-24 18:51:53 +0000

    2372290 Merge 99c46906efb2703d1935540dfb7097f0ed5465a4 into e54b66d241b9a70871eff4281e51fe0965e40a57

---
+++ elements/query-builder/condition-group/condition-group.component.d.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectorRef, OnDestroy, OnInit } from '@angular/core';
+import { ControlContainer, FormArray, FormBuilder, UntypedFormGroup } from '@angular/forms';
+import { QueryBuilderService } from '../query-builder.service';
+import { Condition } from '../query-builder.types';
+import { NovoLabelService } from 'novo-elements/services';
+import * as i0 from "@angular/core";
+export declare class ConditionGroupComponent implements OnInit, OnDestroy {
+    qbs: QueryBuilderService;
+    labels: NovoLabelService;
+    private controlContainer;
+    private formBuilder;
+    private cdr;
+    controlName: string;
+    groupIndex: number;
+    parentForm: UntypedFormGroup;
+    /** Subject that emits when the component has been destroyed. */
+    private readonly _onDestroy;
+    constructor(qbs: QueryBuilderService, labels: NovoLabelService, controlContainer: ControlContainer, formBuilder: FormBuilder, cdr: ChangeDetectorRef);
+    ngOnInit(): void;
+    ngOnDestroy(): void;
+    updateControlName(value: string): void;
+    get root(): FormArray;
+    addCondition(data?: any): void;
+    removeCondition(index: number): void;
+    newCondition({ field, operator, value }?: Condition): UntypedFormGroup;
+    cantRemoveRow(): boolean;
+    static ɵfac: i0.ɵɵFactoryDeclaration<ConditionGroupComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ConditionGroupComponent, "novo-condition-group", never, { "controlName": { "alias": "controlName"; "required": false; }; "groupIndex": { "alias": "groupIndex"; "required": false; }; }, {}, never, never, false, never>;
+}
