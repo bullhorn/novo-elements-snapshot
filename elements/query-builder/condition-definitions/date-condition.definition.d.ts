537b86e679c10daf3aff262b93dfc45590282f2b
Dan Voegelin <dvoegelin@bullhorn.com>
2024-05-28 17:59:57 +0000

    ecdc794 Merge 70dda789529483c3a9baa27ebd49f1e7ee3378ad into ed28bc7b10c2224bd7245d204bb629056ad9e978

---
+++ elements/query-builder/condition-definitions/date-condition.definition.d.ts
@@ -0,0 +1,16 @@
+import { QueryList } from '@angular/core';
+import { NovoPickerToggleElement } from 'novo-elements/elements/field';
+import { AbstractConditionFieldDef } from './abstract-condition.definition';
+import { Operator } from '../query-builder.types';
+import * as i0 from "@angular/core";
+/**
+ * Most complicated of the default conditions defs, a date needs to provide a different
+ * input type depending on the operator selected.
+ */
+export declare class NovoDefaultDateConditionDef extends AbstractConditionFieldDef {
+    overlayChildren: QueryList<NovoPickerToggleElement>;
+    defaultOperator: Operator;
+    closePanel(event: any, viewIndex: any): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<NovoDefaultDateConditionDef, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<NovoDefaultDateConditionDef, "novo-date-condition-def", never, {}, {}, never, never, false, never>;
+}
