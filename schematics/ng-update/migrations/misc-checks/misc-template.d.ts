a49bbf0407906dd6c96d913b4a2ed2e3cfd043a4
cgalvinbullhorn <87441206+cgalvinbullhorn@users.noreply.github.com>
2024-06-05 18:12:10 +0000

    2e2ace5 Merge 104a52946e54a2f5d340194ad5ac6f9d668d2012 into e233914d4b232a432c735ef5bb0aa8b8571a5b2a

--- schematics/ng-update/migrations/misc-checks/misc-template.d.ts
+++
@@ -1,9 +0,0 @@
-import { Migration, ResolvedResource } from '@angular/cdk/schematics';
-/**
- * Migration that walks through every inline or external template and reports if there
- * are outdated usages of the Angular Material API that needs to be updated manually.
- */
-export declare class MiscTemplateMigration extends Migration<null> {
-    enabled: boolean;
-    visitTemplate(template: ResolvedResource): void;
-}
