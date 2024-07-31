b947e4efe37131ba3e8775881844039bace5a366
Tom Fechtner <tom.fechtner@bullhorn.com>
2024-07-31 09:16:21 +0000

    d7daf6f Merge 5a603fd582765c4fef32efd1bd2b60cb4c275bb6 into 159ddd1639fcb023a38f987f2003f2f742c4a8ef

---
+++ elements/modal/modal.service.d.ts
@@ -0,0 +1,18 @@
+import { Overlay } from '@angular/cdk/overlay';
+import { Injector, ViewContainerRef } from '@angular/core';
+import { NovoModalRef } from './modal-ref';
+import * as i0 from "@angular/core";
+export declare class NovoModalService {
+    private injector;
+    private overlay;
+    _parentViewContainer: ViewContainerRef;
+    set parentViewContainer(view: ViewContainerRef);
+    constructor(injector: Injector, overlay: Overlay);
+    open<T extends Record<string, any>>(component: any, params?: Partial<T>): NovoModalRef<Partial<T>, any>;
+    private createOverlay;
+    private attachModalContainer;
+    private createInjector;
+    private getOverlayConfig;
+    static ɵfac: i0.ɵɵFactoryDeclaration<NovoModalService, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<NovoModalService>;
+}
