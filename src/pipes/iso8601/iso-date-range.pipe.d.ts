797772c2f5f313ad9ee308e5e9db90c42a8f5bad
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2024-08-13 15:08:52 +0000

    94b3975 Merge 0e6b8bc20284509bbfa49305cab0d69c9004a4f6 into 1631764d49f44f74ce562886870be1b9be0c3f2a

--- src/pipes/iso8601/iso-date-range.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from '@angular/core';
-declare type IsoDateRangeArgs = (string | Date)[];
-export declare class IsoDateRangePipe implements PipeTransform {
-    constructor();
-    transform(dates: IsoDateRangeArgs): string;
-}
-export {};
