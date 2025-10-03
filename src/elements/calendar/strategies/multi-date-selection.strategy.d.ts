77f24a8f77877e64b26652dc3409522563e11e68
renatamazetto023 <renata.mazetto@bullhorn.com>
2025-10-03 14:33:38 +0000

    7d2a72b Merge 60b1ebbb58fdd5fc4c01746a7bb03915a44a20d0 into 21611e76ae5592ef42854058c6f80dc9b49ed3e2

--- src/elements/calendar/strategies/multi-date-selection.strategy.d.ts
+++
@@ -1,6 +0,0 @@
-import type { DateLike, NovoDateSelectionStrategy } from '../../date-picker/date-picker.types';
-export declare class MultiDateSelectionStrategy implements NovoDateSelectionStrategy<DateLike[]> {
-    selectionFinished(dateLike: DateLike | null, currentValue: DateLike[], event: Event): DateLike[];
-    createPreview(activeDate: DateLike | null, currentValue: DateLike[]): (string | number | Date)[];
-    isSelected(activeDate: DateLike | null, currentValue: DateLike[]): boolean;
-}
