9a38d6d625cad3ca530b25ece09c8bb809aa9a16
Nathan Dickerson <nathan@edickerson.net>
2025-10-28 19:12:00 +0000

    a97685a Merge 4ea22373ce6363b0fb5625a5e58f46a69c92f267 into d40a962ff81849415dd5a86084414ea2cd297440

--- schematics/testing/test-project.d.ts
+++
@@ -1,4 +0,0 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
-/** Create a base project used for testing. */
-export declare function createTestProject(runner: SchematicTestRunner, projectType: 'application' | 'library', appOptions?: {}, tree?: Tree): Promise<UnitTestTree>;
