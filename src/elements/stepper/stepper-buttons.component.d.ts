d4e4facbf0957d3db564f3bbe085524e6c53ff63
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2025-10-28 21:17:26 +0000

    d83140d Merge edf33065f2ab11eb5a98548001e4a6ca781b0a01 into 839d11e8690468c0617b3a0b53d2f66f0e19b50f

--- src/elements/stepper/stepper-buttons.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { CdkStepperNext, CdkStepperPrevious } from '@angular/cdk/stepper';
-/** Button that moves to the next step in a stepper workflow. */
-export declare class NovoStepperNext extends CdkStepperNext {
-}
-/** Button that moves to the previous step in a stepper workflow. */
-export declare class NovoStepperPrevious extends CdkStepperPrevious {
-}
