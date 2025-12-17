6fd4bbc35ca764430e49beab042cc952d88c41dd
Connor-Tyrell <connor.tyrell@bullhorn.com>
2025-12-17 19:16:53 +0000

    ca3f04d Merge 5b08e1d391068f74405757821b2028fb13413abc into f77c0616c6adc4c925b06a45b7884026337028eb

--- schematics/ng-add/fonts/novo-fonts.d.ts
+++
@@ -1,4 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { Schema } from '../schema';
-/** Adds the Material Design fonts to the index HTML file. */
-export declare function addFontsToIndex(options: Schema): Rule;
