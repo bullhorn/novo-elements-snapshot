e96f7003786980dc2d565ada8c56a5095c800d00
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2026-03-23 15:33:48 +0000

    2341e8d Merge ac252976e83bed85267743e3b2e895b7a5ce5cf4 into 7cbe432a07df2fbd4c006d7a3622c891c8c1f07f

---
+++ services/component-utils/ComponentUtils.d.ts
@@ -0,0 +1,7 @@
+import { ComponentRef, StaticProvider, Type, ViewContainerRef } from '@angular/core';
+import * as i0 from "@angular/core";
+export declare class ComponentUtils<T = any> {
+    append<T>(ComponentClass: Type<T>, location: ViewContainerRef, providers?: StaticProvider[], onTop?: boolean): ComponentRef<T>;
+    static ɵfac: i0.ɵɵFactoryDeclaration<ComponentUtils<any>, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<ComponentUtils<any>>;
+}
