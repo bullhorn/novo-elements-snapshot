c3caf52f0b1e0df5d25f729f31b17758916556b0
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2026-04-01 14:24:10 +0000

    ad7a881 Merge 6b36d7f659b1ab6305dab995cb2c359665ad92c2 into 91a97868c7e089dbab77c40724a33a6c512d5ca5

--- src/elements/form/controls/check-list/CheckListControl.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseControl, NovoControlConfig } from './../BaseControl';
-export declare class CheckListControl extends BaseControl {
-    controlType: string;
-    constructor(config: NovoControlConfig);
-}
