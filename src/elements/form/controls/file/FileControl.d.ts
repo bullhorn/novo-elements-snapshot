345cf26b69f206926f53f6985133bff4004c95b6
Peter Monroe <monroepe@gmail.com>
2026-04-10 17:47:06 +0000

    dcfdbd9 Merge 74a0d3e99967fa9d6a9f4d1cab74bf92e38edc70 into d795849244615236657177e29b36a69550932f1a

--- src/elements/form/controls/file/FileControl.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseControl, NovoControlConfig } from './../BaseControl';
-export declare class FileControl extends BaseControl {
-    controlType: string;
-    constructor(config: NovoControlConfig);
-}
