235b073633ded287a06f9c0d89ebcf7d14d9d4a8
Tom Fechtner <tom.fechtner@bullhorn.com>
2026-07-03 15:28:16 +0000

    c4bb7c9 Merge 1d1a84b227eb6cec840abfecab87c2ef3a1bd2d7 into dab958bee798f3dffc6ab260733fbd3aa8c1b576

--- src/elements/common/directives/flex.directive.d.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef, Renderer2 } from '@angular/core';
-export declare class FlexDirective {
-    private readonly el;
-    private readonly renderer;
-    private _flex;
-    get flex(): string;
-    set flex(value: string);
-    constructor(el: ElementRef, renderer: Renderer2);
-}
