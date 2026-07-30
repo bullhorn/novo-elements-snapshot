4b21cf1f2064e81869553be4b0c5b1202d69a54e
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2026-07-30 21:29:27 +0000

    45cc85f Merge c2d6d62a878570b2db39d8ff2ca5cfd3c836c845 into 24eebd3b143ce77aaa3c304c5ee7a89a3a1c568e

--- src/elements/stepper/step-label.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { CdkStepLabel } from '@angular/cdk/stepper';
-import { TemplateRef } from '@angular/core';
-export declare class NovoStepLabel extends CdkStepLabel {
-    constructor(template: TemplateRef<any>);
-}
