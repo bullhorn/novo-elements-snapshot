cb4af987646a638908b6f95c6399379b5879b06a
Peter Monroe <monroepe@gmail.com>
2026-08-04 15:45:59 +0000

    0ab8a73 Merge ebbfeb392bb4340838b2914d0b54aa17c5a78724 into 09e673f2e166a7f1db9bd77ec6305cb9ebbcdfbe

--- schematics/paths.d.ts
+++
@@ -1,4 +0,0 @@
-/** Path to the schematic collection for non-migration schematics. */
-export declare const COLLECTION_PATH: string;
-/** Path to the schematic collection that includes the migrations. */
-export declare const MIGRATION_PATH: string;
