3eecb5a6b4644bc3caff646c9153f6424a78b2b5
Peter Monroe <monroepe@gmail.com>
2026-09-04 13:51:17 +0000

    ef6bc5b Merge 50202979bd33c313295688250dc2a4ff35e1a560 into 01685da52b12ffda50cb6a1aa7da4871590fcf5c

--- src/elements/form/controls/date/DateControl.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseControl, NovoControlConfig } from './../BaseControl';
-export declare class DateControl extends BaseControl {
-    controlType: string;
-    constructor(config: NovoControlConfig);
-}
