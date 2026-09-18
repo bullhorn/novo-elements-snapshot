27b3a643c3286a6f7305582cb942d1bde532db8e
David Keyworth <130384363+davidkbh@users.noreply.github.com>
2026-09-18 15:07:24 +0000

    e7d37f5 Merge 63ed3bf4bea07e4d818c1ae68fff73960ac16732 into da0ad416dbe88f0ebb759a6d3d690fe191a2724a

--- src/elements/field/formats/base-format.d.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter, InjectionToken } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
-export declare const NOVO_INPUT_FORMAT: InjectionToken<NovoInputFormat<any>>;
-export interface NovoInputFormat<T = any> extends ControlValueAccessor {
-    valueChange: EventEmitter<any>;
-    formatValue(value: T): string;
-}
