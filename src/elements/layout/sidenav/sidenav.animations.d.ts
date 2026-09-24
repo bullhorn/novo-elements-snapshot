e0a3d198cccfe3697ee9bfc52d58201b01e207f5
Nathan Dickerson <nathan.dickerson@bullhorn.com>
2026-09-24 16:35:38 +0000

    3d5b862 Merge 3f9a7a3ffcab4d5118582ff1b4a2841af6e04747 into c10abc5213efaa39cfd4e074952f7a73a199a504

--- src/elements/layout/sidenav/sidenav.animations.d.ts
+++
@@ -1,8 +0,0 @@
-import { AnimationTriggerMetadata } from '@angular/animations';
-/**
- * Animations used by the Material drawers.
- * @docs-private
- */
-export declare const novoSidenavAnimations: {
-    readonly transformDrawer: AnimationTriggerMetadata;
-};
